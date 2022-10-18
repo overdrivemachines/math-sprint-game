b606cb08cacaacd36d4a4a33de65259e03153262
get.dipen@gmail.com <get.dipen@gmail.com>
2022-10-17 22:05:23 -0700

    completed score page

--- script.js
+++ script.js
@@ -52,6 +52,41 @@ let finalTimeDisplay = "0.0s";
 // Scroll
 let valueY = 0;
 
+// Reset Game. When play again button is clicked.
+function playAgain() {
+  gamePage.addEventListener("click", startTimer);
+  scorePage.hidden = true;
+  splashPage.hidden = false;
+  equationsArray = [];
+  playerGuessArray = [];
+  valueY = 0;
+  playAgainBtn.hidden = true;
+}
+
+// Show Score Page
+function showScorePage() {
+  // Show play again button after 1 second
+  setTimeout(() => {
+    playAgainBtn.hidden = false;
+  }, 1000);
+  gamePage.hidden = true;
+  scorePage.hidden = false;
+}
+
+// Format & Display Time in DOM
+function scoresToDOM() {
+  finalTimeDisplay = finalTime.toFixed(1);
+  baseTime = timePlayed.toFixed(1);
+  penaltyTime = penaltyTime.toFixed(1);
+
+  baseTimeEl.textContent = `Base Time: ${baseTime}s`;
+  penaltyTimeEl.textContent = `Penalty: +${penaltyTime}s`;
+  finalTimeEl.textContent = `${finalTimeDisplay}s`;
+  // Scroll to Top, go to Score page
+  itemContainer.scrollTo({ top: 0, behavior: "instant" });
+  showScorePage();
+}
+
 // Stop Timer, Process Results, go to Score Page
 function checkTime() {
   if (playerGuessArray.length == questionAmount) {
@@ -72,6 +107,7 @@ function checkTime() {
     }
     finalTime = timePlayed + penaltyTime;
     console.log("time played", timePlayed, "penalty:", penaltyTime, "final:", finalTime);
+    scoresToDOM();
   }
 }
 
